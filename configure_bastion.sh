b9df365abc112a7f1dfc054648d721da0a37ae5d
Gianni Salinetti <gbsalinetti@extraordy.com>
2018-06-01 09:50:39 +0200

    Update bastion config script

--- configure_bastion.sh
+++ configure_bastion.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+REPOS="epel-release centos-release-openshift-origin39"
 EXTRA_PKG="git vim-enhanced bind-utils wget bash-completion"
 
 # This script must be run as root
@@ -8,17 +9,19 @@ if [ $USER != root ]; then
     exit 1
 fi
 
+# Configure repositories
+for repo in $REPOS; do 
+    yum install -y $repo
+    if [ $? -ne 0 ]; then
+        echo "Error configuring repository $repo"
+        exit 1
+    fi
+done
+
 # Update the system
 yum update -y
 if [ $? -ne 0 ]; then
     echo "Cannot update system to latest packages"
-    exit 1
-fi
-
-# Install ansible on bastion host
-yum install -y ansible
-if [ $? -ne 0 ]; then
-    echo "Error installing ansible"
     exit 1
 fi
 
@@ -31,3 +34,11 @@ for pkg in $EXTRA_PKG; do
     fi
 done
 
+# Install atomic-openshift-utils on bastion host
+yum install -y atomic-openshift-utils
+if [ $? -ne 0 ]; then
+    echo "Error installing package atomic-openshift-utils"
+    exit 1
+fi
+
+exit 0
