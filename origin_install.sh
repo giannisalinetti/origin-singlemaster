6e68ab8a6cc4b0d0a9c1d8078e3b5e70843e7003
Gianni Salinetti <gbsalinetti@extraordy.com>
2018-06-01 10:14:21 +0200

    Update install script

--- origin_install.sh
+++ origin_install.sh
@@ -2,9 +2,15 @@
 
 INVENTORY='inventory/singlemaster'
 
-ansible-playbook -i ${INVENTORY} /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml
+ansible-playbook -i ${INVENTORY} /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
 if [ $? -ne 0 ]; then
-    echo "Error: OpenShift installation failed"
+    printf 'Error during OCP 3.9 prerequisites configuration.\f'
+    exit 1
+fi
+  
+ansible-playbook -i ${INVENTORY} /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
+if [ $? -ne 0 ]; then
+    printf 'Error during OCP 3.9 cluster deployment.\f'
     exit 1
 fi
 
